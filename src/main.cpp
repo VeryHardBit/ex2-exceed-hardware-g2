e7af35b2547eb3377299623b6b02c849fefe459b
VeryHardBit <yanothaic15@gmail.com>
2023-02-06 15:32:15 +0700

    fix nearby GET bug

--- src/main.cpp
+++ src/main.cpp
@@ -18,32 +18,28 @@ Bounce debouncer = Bounce();
 
 void Connect_Wifi();
 
-void setup()
-{
-  Serial.begin(115200);
-  pinMode(red, OUTPUT);
-  pinMode(yellow, OUTPUT);
-  pinMode(green, OUTPUT);
-  pinMode(ldr, INPUT);
-  debouncer.attach(button, INPUT_PULLUP);
-  debouncer.interval(25);
-  Connect_Wifi();
+void setup(){
+    Serial.begin(115200);
+    pinMode(red, OUTPUT);
+    pinMode(yellow, OUTPUT);
+    pinMode(green, OUTPUT);
+    pinMode(ldr, INPUT);
+    debouncer.attach(button, INPUT_PULLUP);
+    debouncer.interval(25);
+    Connect_Wifi();
 
-  delay(200);
-  // start LED with GREEN and POST to database
-  digitalWrite(green, HIGH);
-  POST_traffic("green");
+    delay(200);
+    // start LED with GREEN and POST to database
+    digitalWrite(green, HIGH);
+    POST_traffic("green");
 }
 
 long int time0 = 0;
 long int time1 = 0;
 void loop(){
-  // *** write your code here ***
-  // Your can change everything that you want
     debouncer.update();
     bool btn_pressed=debouncer.fell();
-    if (state == 1){
-        // while led GREEN
+    if (state == 1){//green state
         digitalWrite(green, HIGH);
         if(btn_pressed){
             state=2;
@@ -51,9 +47,7 @@ void loop(){
             POST_traffic("yellow");
             time0=millis();
         }
-    }
-    else if (state == 2){
-        // while led YELLOW
+    }else if (state == 2){//yellow state
         digitalWrite(yellow, HIGH);
         if(millis()-time0>8000){
             state=3;
@@ -62,8 +56,7 @@ void loop(){
             GET_traffic();
             time0=millis();
         }
-    }else if (state == 3){
-        // while led RED
+    }else if (state == 3){//red state
         digitalWrite(red, HIGH);
         if(millis()-time0>5000){
             while(1){
@@ -81,16 +74,15 @@ void loop(){
 }
 
 void Connect_Wifi(){
-  const char *ssid = "OPPO_KUY";
-  const char *password = "oppopass";
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.print("OK! IP=");
-  Serial.println(WiFi.localIP());
-  Serial.println("----------------------------------");
+    const char *ssid = "OPPO_KUY";
+    const char *password = "oppopass";
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi");
+    while (WiFi.status() != WL_CONNECTED){
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.print("OK! IP=");
+    Serial.println(WiFi.localIP());
+    Serial.println("----------------------------------");
 }
--- src/traffic.h
+++ src/traffic.h
@@ -9,68 +9,61 @@ const String point = "2";
 const int nearby_1 = 1;
 const int nearby_2 = 3;
 
-void GET_traffic()
-{
-  DynamicJsonDocument doc(65536);
-  HTTPClient http;
-  const String url = baseUrl + "/all_traffic";
-  http.begin(url);
+void GET_traffic(){
+    DynamicJsonDocument doc(65536);
+    HTTPClient http;
+    const String url = baseUrl + "/all_traffic";
+    http.begin(url);
 
-  Serial.println("Nearby traffic");
-  int httpResponseCode = http.GET();
-  if (httpResponseCode >= 200 && httpResponseCode < 300) {
-    Serial.print("HTTP ");
-    Serial.println(httpResponseCode);
-    String payload = http.getString();
-    deserializeJson(doc, payload);
+    Serial.println("Nearby traffic");
+    int httpResponseCode = http.GET();
+    if (httpResponseCode >= 200 && httpResponseCode < 300) {
+        Serial.print("HTTP ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        deserializeJson(doc, payload);
 
-    // *** write your code here ***
-    // set up JSON
-    Serial.println();
-    Serial.println(nearby_1);
-    Serial.println((const char*)doc["all_traffic"][nearby_1]["traffic"]);
-    Serial.println();
-    Serial.println(nearby_2);
-    Serial.println((const char*)doc["all_traffic"][nearby_2]["traffic"]);
+        // *** write your code here ***
+        // set up JSON
+        Serial.println();
+        Serial.println(nearby_1);
+        Serial.println((const char*)doc["all_traffic"][nearby_1-1]["traffic"]);
+        Serial.println();
+        Serial.println(nearby_2);
+        Serial.println((const char*)doc["all_traffic"][nearby_2-1]["traffic"]);
    
-  }
-  else
-  {
-    Serial.print("Error ");
-    Serial.println(httpResponseCode);
-  }
+    }else{
+        Serial.print("Error ");
+        Serial.println(httpResponseCode);
+    }
 
-  Serial.println("----------------------------------");
+    Serial.println("----------------------------------");
 }
 
-void POST_traffic(String led)
-{
-  const String url = baseUrl + "/my_traffic?point=" + point;
-  String json;
-  HTTPClient http;
-  http.begin(url);
-  http.addHeader("Content-Type", "application/json");
+void POST_traffic(String led){
+    const String url = baseUrl + "/my_traffic?point=" + point;
+    String json;
+    HTTPClient http;
+    http.begin(url);
+    http.addHeader("Content-Type", "application/json");
 
-  DynamicJsonDocument doc(2048);
-  // *** write your code here ***
-  doc["code"] = "zgam1";
-  doc["traffic"] = led;
-  serializeJson(doc, json);
+    DynamicJsonDocument doc(2048);
+    // *** write your code here ***
+    doc["code"] = "zgam1";
+    doc["traffic"] = led;
+    serializeJson(doc, json);
 
-  Serial.println("POST " + led);
-  int httpResponseCode = http.POST(json);
-  if (httpResponseCode == 200)
-  {
-    Serial.print("HTTP ");
-    Serial.println(httpResponseCode);
-    Serial.print("Done");
-    Serial.println();
-  }
-  else
-  {
-    Serial.print("Error ");
-    Serial.println(httpResponseCode);
-  }
+    Serial.println("POST " + led);
+    int httpResponseCode = http.POST(json);
+    if (httpResponseCode == 200){
+      Serial.print("HTTP ");
+      Serial.println(httpResponseCode);
+      Serial.print("Done");
+      Serial.println();
+    }else{
+      Serial.print("Error ");
+      Serial.println(httpResponseCode);
+    }
 
-  Serial.println("----------------------------------");
+    Serial.println("----------------------------------");
 }
